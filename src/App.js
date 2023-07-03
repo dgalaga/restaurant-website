d6e9b883404738152b5a4f9450c57b187b032043
dgalaga <57417236+dgalaga@users.noreply.github.com>
2023-07-03 16:27:30 -0700

    import HomePage from './components/pages/HomePage'; from import HomePage from './components/HomePage';

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import HomePage from './components/HomePage';
+import HomePage from './components/pages/HomePage';
 import Routing from "./components/Routing";
 import Footer from "./components/Footer";
 import GoogleMap from "./components/googlemaps";
