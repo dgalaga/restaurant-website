062f22966e033124e96c42dbd17000f9a04a4afb
dgalaga <57417236+dgalaga@users.noreply.github.com>
2023-07-03 16:24:24 -0700

    reroute to make HomePage.js be the default

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import HomePage from './components/HomePage';
 import Routing from "./components/Routing";
 import Footer from "./components/Footer";
 import GoogleMap from "./components/googlemaps";
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <HomePage />
       <Routing />
       <Footer />
       <GoogleMap />
