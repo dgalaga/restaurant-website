1aab260f8a07606fb3b4a8f60ea2bf68f6e94251
dgalaga <57417236+dgalaga@users.noreply.github.com>
2023-07-03 21:55:36 -0700

    change path of /

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/HomePage";
 import About from "./pages/About";
 import Reservation from "./pages/BookingPage";
@@ -9,17 +9,12 @@ import Confirmation from "./pages/Confirmation";
 export default function Routing() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-
+      <Route path="/" element={<Homepage />} />
       <Route path="/home" element={<Homepage />} />
-
       <Route path="/about" element={<About />} />
-
       <Route path="/reservations" element={<Reservation />} />
-
       <Route path="/order" element={<Order />} />
       <Route path="/login" element={<Login />} />
-
       <Route path="/confirmation" element={<Confirmation />} />
     </Routes>
   );
